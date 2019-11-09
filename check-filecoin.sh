878eae94415d253260c64d2a2e44968540a938a2
lengbo@storswift.com <lengbo@storswift.com>
2019-11-09 20:04:49 +0800

    Start mining with more efficient method.

--- check-filecoin.sh
+++ check-filecoin.sh
@@ -15,11 +15,15 @@ mkdir -p ${LOGDIR}
 
 # echo 3 >/proc/sys/vm/drop_caches
 
-PREV_PID=`ps -ef | grep "go-filecoin --repodir=${FILECOIN_REPO}" |grep -v grep | awk '{print $2}'`
+PREV_PID=`ps -ef | grep "go-filecoin --repodir=${FILECOIN_REPO} daemon" |grep -v grep | awk '{print $2}'`
 if [ "${PREV_PID}" = "" ]; then
     logger "Restart filecoin daemon."
     nohup ${BINDIR}/go-filecoin --repodir=${FILECOIN_REPO} daemon 1>${LOGDIR}/filecoin_`date +%Y%m%d%H%M`.log 2>&1 &
-    sleep 300
-    echo `date` >> ${LOGDIR}/mining_status
-    ${BINDIR}/go-filecoin --repodir=${FILECOIN_REPO} mining start 1>>${LOGDIR}/mining_status 2>&1
+	${BINDIR}/go-filecoin --repodir=${FILECOIN_REPO} mining start 1>>${LOGDIR}/mining_status 2>&1
+	MINING_STATUS=`go-filecoin --repodir=${FILECOIN_REPO}  mining status | grep Active | awk '{print $2}'`
+	while [ "${MINING_STATUS}" != "true" ]; do
+		${BINDIR}/go-filecoin --repodir=${FILECOIN_REPO} mining start 1>>${LOGDIR}/mining_status 2>&1
+		sleep 1
+		MINING_STATUS=`go-filecoin --repodir=${FILECOIN_REPO}  mining status | grep Active | awk '{print $2}'`
+	done
 fi
